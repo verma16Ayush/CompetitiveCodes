edb43ae90f67907cc294c480c2a2e8557caa02b0
Ayush <verma16.ayush@gmail.com>
2020-11-15 16:50:23 +0530

    more stuff

--- leetcode/permutations2.cpp
+++ leetcode/permutations2.cpp
@@ -8,14 +8,14 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef map<int, int> mii;
+// typedef map<int, int> mii;
 typedef pair<int, int> pii;
 typedef vector<vector<int>> int_vec_2d;
 typedef vector<int>:: iterator vi_it;
-typedef map<int, int>::iterator mii_it;
+// typedef map<int, int>::iterator mii_it;
 
 
 class Solution 
